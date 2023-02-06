041969102df4b8bc00e62e42429835524a676058
Zireael112 <86360076+Zireael112@users.noreply.github.com>
2023-02-06 16:44:00 +0300

    Update task.js

--- tooltip/task.js
+++ tooltip/task.js
@@ -1,26 +1,26 @@
-const tooltips = document.querySelectorAll('.has-tooltip');
+const tooltips = document.querySelectorAll('.has-tooltip')
 
 function setTooltipActive() {
-    const tooltip = document.getElementsByClassName('tooltip_active')[0]; 
+    const tooltip = document.getElementsByClassName('tooltip_active')[0]
   
     event.preventDefault(); 
   
     if (tooltip !== undefined) {
-        tooltip.classList.remove('tooltip_active');
+        tooltip.classList.remove('tooltip_active')
             if (tooltip.textContent == this.title) {
                 return false;
               }
             }
   
-    const createEl = document.createElement('div');
-    createEl.classList.add('tooltip', 'tooltip_active');
-    createEl.textContent = this.getAttribute('title');
-    const position= this.getBoundingClientRect();
-    createEl.style.top = position.bottom + 3 + 'px';
-    createEl.style.left = position.left + 'px';
+    const createEl = document.createElement('div')
+    createEl.classList.add('tooltip', 'tooltip_active')
+    createEl.textContent = this.getAttribute('title')
+    const position= this.getBoundingClientRect()
+    createEl.style.top = position.bottom + 3 + 'px'
+    createEl.style.left = position.left + 'px'
     document.body.append(createEl); 
   }
 
 for (let element of tooltips) {
-    element.addEventListener('click', setTooltipActive);
+    element.addEventListener('click', setTooltipActive)
 }
